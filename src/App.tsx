ced15a59cb7d3df3e99d88d83c533fed388e4a55
fabiovasques87 <fabioportovasques@yahoo.com.br>
2023-01-02 17:14:13 -0300

    add logo jmu na nav menu amburger mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import {useRoutes, Link} from 'react-router-dom';
-import logoJmu from './assets/logoJmu.png';
 // import { lists } from './helpers/Dados';
 import {MyRoutes} from './routes/MyRoutes';
 import Style  from './styles.module.css';
@@ -15,6 +14,7 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import { Button } from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
 import Collapse from 'react-bootstrap/Collapse';
+import logoJmu from './assets/logoJmu.png';
 
 const App = () =>{
 
@@ -30,7 +30,7 @@ const App = () =>{
     
       <div className={Style.header}>
             <Link to={'/'} className={Style.iconHouse}><FontAwesomeIcon icon={faHouse}  /> </Link>  
-            <h1 className={Style.textHeader}>Equipamentos de Informática</h1>   
+            <h1 className={Style.textHeader}>Equipamentos de Informática JMU</h1>   
             <DropdownButton className={Style.dropdownPainelControle} id="dropdown-item-button"   variant="success" title="Painel de Controle">
               <Link to={'/'} className={Style.editarItens}><Dropdown.ItemText ><FontAwesomeIcon className={Style.iconMenuPainelCOntrole} icon={faHouse}  />Home</Dropdown.ItemText></Link>
                 <Link to={'/plenario'} className={Style.editarItens}><Dropdown.ItemText ><FontAwesomeIcon className={Style.iconMenuPainelCOntrole} icon={faRightToBracket}  />Plenário</Dropdown.ItemText></Link>
@@ -50,6 +50,7 @@ const App = () =>{
             <nav className={Style.Nav}>              
              <ul>
               
+               
                 <Link  className={Style.linkMenuAmburger} to={'/'}><li> <FontAwesomeIcon className={Style.iconMenuAmburguer} icon={faHouse}  /> Home</li> </Link>  
                 <Link className={Style.linkMenuAmburger} to={'/plenario'}><li><FontAwesomeIcon className={Style.iconMenuAmburguer} icon={faRightToBracket}  /> Plenário</li> </Link>  
                 <Link className={Style.linkMenuAmburger} to={'/adm'}><li><FontAwesomeIcon className={Style.iconMenuAmburguer} icon={faRightToBracket}  />Administrativo</li> </Link>  
@@ -57,7 +58,9 @@ const App = () =>{
                 <Link className={Style.linkMenuAmburger} to={'/video'}><li><FontAwesomeIcon className={Style.iconMenuAmburguer} icon={faRightToBracket}  />Vídeo Conferência</li> </Link>
                 <Link  className={Style.linkMenuAmburger} to={'/login'}><li> <FontAwesomeIcon className={Style.iconMenuAmburguer} icon={faPlus}  /> Adiconar Item</li> </Link> 
                 <Link  className={Style.linkMenuAmburger} to={'/SearchItem'}><li> <FontAwesomeIcon className={Style.iconMenuAmburguer} icon={faSearch}  /> Pesquisar Item</li> </Link>  
- 
+                <>
+                  <div className={Style.logoJmu}><img  src={logoJmu} alt="" /></div>
+                </>
   
 
 
